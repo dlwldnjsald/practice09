ae0f6c1846fb5f917845d3e0fe14dcce4761cdf5
806-26 <806-26@DESKTOP-8V02G4F>
2024-04-11 10:03:37 +0900

    commit and push on 0411 10:03

--- src/com/javaex/ex04/RectTriangle.java
+++ src/com/javaex/ex04/RectTriangle.java
@@ -1,8 +1,48 @@
 package com.javaex.ex04;
 
-public class RectTriangle {
-    private double width;
-    private double height;
 
-    
-}
+
+public class RectTriangle extends Shape{
+
+
+
+//필드
+
+	private double width;
+
+	private double height;
+
+	
+
+	//생성자
+
+	public RectTriangle(double width, double height) {
+
+		this.width	= width;
+
+		this.height = height;
+
+	}
+
+	//alt shift s source override implement method 
+	//getArea, get Perimeter()_ 기능 자동 복붙하기)
+
+	@Override
+	public double getArea() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+
+	@Override
+	public double getPerimeter() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	
+
+
+
+	
--- src/com/javaex/ex04/Rectangle.java
+++ src/com/javaex/ex04/Rectangle.java
@@ -1,7 +1,63 @@
 package com.javaex.ex04;
 
-public class Rectangle {
-    private double width;
-    private double height;
+
+
+//resizeable이라 하는 인터페이스 구현
+
+public class Rectangle extends Shape implements Resizeable {
+
+
+
+//필드
+
+	private double width;
+
+	private double height;
+
+
+
+//생성자
+
+	public Rectangle(double width,double height) {
+
+	this.width = width;
+
+	this.height = height;
+
+	}
+
+//als shift s source override implement method하기
+
+	@Override
+
+	public void resize(double s) {
+
+	width = width* s;
+
+	height = height* s;
+
+
+
+	@Override
+
+	public double getArea() {
+
+	return width * height;
+
+	}
+
+	
+
+	@Override
+
+	public double getPerimeter() {
+
+	return 2 * (width + height);
+
+	}
+
+
+
+	
 
 }
